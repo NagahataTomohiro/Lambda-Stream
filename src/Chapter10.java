971c847493823b5edb6ce29288603193a73b4711
N.Tomohiro <NagahataTomohiro@users.noreply.github.com>
2023-02-06 19:09:19 +0900

    課題提出

--- src/Chapter10.java
+++ src/Chapter10.java
@@ -11,21 +11,13 @@ public class Chapter10 {
 		list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
 		list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
 
-		int count = 0;
-		for (Task task : list) {
-			if (task.isDone()) {
-				count++;
-			}
-		}
-
+		int count = (int) list.stream().filter(task -> !task.isDone()).count();
 		System.out.println("未完了のタスクの個数は" + count);
 
-		System.out.println("【未完了のタスクを昇順に並べ替えて一覧表示】");
-		list.sort(Task::compareTo);
-		for (Task task : list) {
-			if (task.isDone()) {
-				System.out.println(task);
-			}
-		}
+		System.out.println("【未完了のタスクを昇順に並び替えて一覧表示】");
+		list.stream()
+		    .filter(task -> !task.isDone())
+		    .sorted()
+		    .forEach(task -> System.out.println(task));
 	}
 }
